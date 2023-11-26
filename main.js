6fc78f411fc84d1a80adbc86c7bdf81e37159df0
AndreyUA <infernalsteel666@gmail.com>
2023-11-26 14:40:06 +0200

    loaded all assets

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import "./style.css";
 
 import Phaser from "phaser";
 
+import sky from "/assets/sky.png";
+import star from "/assets/star.png";
+import bomb from "/assets/bomb.png";
+import platform from "/assets/platform.png";
+import dude from "/assets/dude.png";
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -15,7 +21,16 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("sky", sky);
+  this.load.image("star", star);
+  this.load.image("bomb", bomb);
+  this.load.image("platform", platform);
+  this.load.spritesheet("dude", dude, {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
 
 function create() {}
 
