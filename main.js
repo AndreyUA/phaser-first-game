aabe73c8ee3f9f326077e3b8d2fa035ce4fa0280
AndreyUA <infernalsteel666@gmail.com>
2023-11-26 23:26:12 +0200

    added platforms and player

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ import bomb from "/assets/bomb.png";
 import platform from "/assets/platform.png";
 import dude from "/assets/dude.png";
 
+let player, platforms;
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -16,6 +18,13 @@ const config = {
     preload,
     create,
     update,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
   },
 };
 
@@ -34,6 +43,34 @@ function preload() {
 
 function create() {
   this.add.image(400, 300, "sky");
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, "platform").setScale(2).refreshBody();
+  platforms.create(600, 400, "platform");
+  platforms.create(50, 250, "platform");
+  platforms.create(750, 220, "platform");
+
+  player = this.physics.add.sprite(100, 450, "dude");
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
 }
 
 function update() {}
